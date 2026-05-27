df9a69f43265a843544f1af17e76310f845077df
Bertalan Fodor <fodber@organum.hu>
2026-05-27 11:52:11 +0200

    Add hotkey handler to prevent default search shortcut in VitePress

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -4,9 +4,25 @@ import AretinoOnlineEditor from './AretinoOnlineEditor.vue'
 import GabcTranscriptionTool from './GabcTranscriptionTool.vue'
 import './style.css'
 
+function ignorePlainSlashSearchHotkey(event: KeyboardEvent) {
+  if (
+    event.key === '/' &&
+    !event.ctrlKey &&
+    !event.metaKey &&
+    !event.altKey
+  ) {
+    // VitePress local search hard-codes "/" as a global shortcut.
+    event.stopImmediatePropagation()
+  }
+}
+
 export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('keydown', ignorePlainSlashSearchHotkey)
+    }
+
     app.component('AretinoEditor', AretinoEditor)
     app.component('AretinoOnlineEditor', AretinoOnlineEditor)
     app.component('GabcTranscriptionTool', GabcTranscriptionTool)
